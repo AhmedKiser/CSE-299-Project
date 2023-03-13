e0e9ccf7eb7aa7503386f39749d560ffa2edd8b2
AhmedKiser <ahmed.kiser@northsouth.edu>
2023-03-13 11:20:40 +0600

    migration completed

--- Backend/DigitalKrisi/users/models.py
+++ Backend/DigitalKrisi/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    uID = models.IntegerField()
+    uName = models.CharField(max_length=40)
+    uMail = models.EmailField(max_length=30)
+    uPhone = models.TextField(max_length= 15)
+    uDOB = models.DateField()
+    uPassword = models.CharField(max_length=40)
+
