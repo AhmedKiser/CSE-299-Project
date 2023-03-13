b08c365ca6310d2949c0de93ba572216f18322ac
AhmedKiser <ahmed.kiser@northsouth.edu>
2023-03-13 11:43:38 +0600

    Database Created

--- Backend/DigitalKrisi/users/models.py
+++ Backend/DigitalKrisi/users/models.py
@@ -5,7 +5,7 @@ class user(models.Model):
     uID = models.IntegerField()
     uName = models.CharField(max_length=40)
     uMail = models.EmailField(max_length=30)
-    uPhone = models.TextField(max_length= 15)
+    uPhone = models.CharField(max_length=15)
     uDOB = models.DateField()
     uPassword = models.CharField(max_length=40)
 
