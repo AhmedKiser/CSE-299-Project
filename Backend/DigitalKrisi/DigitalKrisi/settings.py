72aeec8d4678c6c7a21fe99083d99f2024198471
AhmedKiser <ahmed11kaiser@gmail.com>
2023-03-20 23:03:08 +0600

    all auth added

--- Backend/DigitalKrisi/DigitalKrisi/settings.py
+++ Backend/DigitalKrisi/DigitalKrisi/settings.py
@@ -42,7 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +136,11 @@ STATICFILES_DIRS = [STATIC_DIR,]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTHENTICATION_BACKENDS = [
+    
+    'django.contrib.auth.backends.ModelBackend', 
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
--- Backend/DigitalKrisi/DigitalKrisi/urls.py
+++ Backend/DigitalKrisi/DigitalKrisi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users import views
 
 urlpatterns = [
@@ -22,4 +22,5 @@ urlpatterns = [
     path('',views.landingPage),
     path('login/',views.LoginUser, name='login'),
     path('registration/',views.registration,name='registration'),
+    path('accounts/', include('allauth.urls')),
 ]
